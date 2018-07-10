21c74344567a5ba6915ef7c1083b2f77f1e6b1d1
Avraam Mavridis <avraammauridis@gmail.com>
2018-07-10 08:01:41 +0200

    Update solution.rb

--- scrolling_text/solution.rb
+++ scrolling_text/solution.rb
@@ -1,6 +1,4 @@
 def scrolling_text(text)
   text = text.upcase
-  text.chars().each_index.map { |i|
-    text.slice(i..text.length - 1) + text.slice(0,i)
-  }
+  text.chars().each_index.map { |i| text.slice(i..text.length - 1) + text.slice(0,i)}
 end
